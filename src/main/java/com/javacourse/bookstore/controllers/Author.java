4a198233007241b96aa2fb484cff508f621fd6a3
Maksymkvn <kvasnikovmv@gmail.com>
2022-09-19 17:23:58 +0300

    First looks class Author and Book

---
+++ src/main/java/com/javacourse/bookstore/controllers/Author.java
@@ -0,0 +1,11 @@
+package com.javacourse.bookstore.controllers;
+
+public class Author {
+    String firstName;
+    String lastName;
+    String surName;
+    int dateOfBirth;
+    int dateOfDeath;
+    String countryOfBirth;
+    String biography;
+}
---
+++ src/main/java/com/javacourse/bookstore/controllers/Book.java
@@ -0,0 +1,24 @@
+package com.javacourse.bookstore.controllers;
+
+public class Book {
+    String title;
+    String author;
+    String series;
+    String cover;
+    String translator;
+    String genre;
+    String publishingHouse;
+    String language;
+    String paper;
+    String annotation;
+    int circulation;
+    String illustrations;
+    int size;
+    int yearOfPublication;
+    double price;
+    int vat;
+    int isbn;
+    int barCode;
+    int id;
+    int pages;
+}
