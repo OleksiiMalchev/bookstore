a3dd0eeaba87b4fb780d19e6853fa7895b3e641d
OleksiiMalchev <oleksiimalchev@gmail.com>
2022-09-19 14:23:17 +0300

    init commit2

--- src/test/java/com/javacourse/bookstore/BookstoreApplicationTests.java
+++ src/test/java/com/javacourse/bookstore/BookstoreApplicationTests.java
@@ -3,6 +3,7 @@ package com.javacourse.bookstore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class BookstoreApplicationTests {
 
