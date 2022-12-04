99580115c8e3b80647fd212f2917aeceb37796dd
Maksymkvn <kvasnikovmv@gmail.com>
2022-12-04 19:33:31 +0200

    Migration correction 04.12.2022

--- src/main/resources/db.migration/V04__bookstore_customer.sql
+++ src/main/resources/db.migration/V04__bookstore_customer.sql
@@ -3,6 +3,6 @@ CREATE TABLE `customer` (
   `first_name` varchar(128) DEFAULT NULL,
   `last_name` varchar(128) DEFAULT NULL,
   `date_of_birth` date DEFAULT NULL,
-  `gender` varchar(128) date DEFAULT NULL,
-  PRIMARY KEY (`id`),
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `gender` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/main/resources/db.migration/V05__bookstore_order.sql
+++ src/main/resources/db.migration/V05__bookstore_order.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS `order` (
+CREATE TABLE `order` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `customer_id` bigint NOT NULL,
   `order_status_id` bigint NOT NULL,
@@ -7,10 +7,9 @@ CREATE TABLE IF NOT EXISTS `order` (
   `change_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `my_first_constraint` (`customer_id`),
-  CONSTRAINT `my_first_constraint` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
+  CONSTRAINT `my_first_constraint` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`),
   KEY `my_second_constraint` (`order_status_id`),
-  CONSTRAINT `my_second_constraint` FOREIGN KEY (`order_status_id`) REFERENCES `order_status` (`id`)
+  CONSTRAINT `my_second_constraint` FOREIGN KEY (`order_status_id`) REFERENCES `order_status` (`id`),
   KEY `my_third_constraint` (`shipment_id`),
   CONSTRAINT `my_third_constraint` FOREIGN KEY (`shipment_id`) REFERENCES `shipment` (`id`)
-
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
