89da7253e4dfc312b7d20ba5e12a470b645eb849
Maksymkvn <kvasnikovmv@gmail.com>
2022-12-04 18:34:59 +0200

    Migration correction 04.12.2022

--- src/main/resources/db.migration/V05_bookstore_order.sql
+++ src/main/resources/db.migration/V05_bookstore_order.sql
@@ -1,7 +1,8 @@
-CREATE TABLE `order` (
+CREATE TABLE IF NOT EXISTS `order` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `customer_id` bigint NOT NULL,
   `order_status_id` bigint NOT NULL,
+  `shipment_id` bigint NOT NULL,
   `created_at` datetime DEFAULT NULL,
   `change_at` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
@@ -9,4 +10,7 @@ CREATE TABLE `order` (
   CONSTRAINT `my_first_constraint` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
   KEY `my_second_constraint` (`order_status_id`),
   CONSTRAINT `my_second_constraint` FOREIGN KEY (`order_status_id`) REFERENCES `order_status` (`id`)
+  KEY `my_third_constraint` (`shipment_id`),
+  CONSTRAINT `my_third_constraint` FOREIGN KEY (`shipment_id`) REFERENCES `shipment` (`id`)
+
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/main/resources/db.migration/V07_bookstore_order_status.sql
+++ src/main/resources/db.migration/V07_bookstore_order_status.sql
@@ -1,8 +1,5 @@
-CREATE TABLE `order_status` (
+CREATE TABLE IF NOT EXISTS `order_status` (
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `order_id` bigint NOT NULL,
   `name_status` varchar(128) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `my_first_constraint` (`order_id`),
-  CONSTRAINT `my_first_constraint` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
