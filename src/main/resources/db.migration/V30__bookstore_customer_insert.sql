6a4b84a06706c5a5a4c229e2de8280293e1b1ddf
Maksymkvn <kvasnikovmv@gmail.com>
2022-12-07 17:28:49 +0200

    Made full migration 07.12.2022

---
+++ src/main/resources/db.migration/V30__bookstore_customer_insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO customer (first_name,last_name,gender) VALUES ('Valeriy','Zaluzhnyi','M');
+INSERT INTO customer (first_name,last_name,gender) VALUES ('Volodymyr','Velykyi','M');
+INSERT INTO customer (first_name,last_name,gender) VALUES ('Larysa','Kot','F');
+INSERT INTO customer (first_name,last_name,gender) VALUES ('Mariya','Shaguri','F');
