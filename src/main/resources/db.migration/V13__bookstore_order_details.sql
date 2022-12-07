ded25776d5483207371a05153c6ecd468c567a36
Maksymkvn <kvasnikovmv@gmail.com>
2022-12-07 16:42:42 +0200

    Made migration2 07.12.2022

--- src/main/resources/db.migration/V13__bookstore_order_details.sql
+++ src/main/resources/db.migration/V13__bookstore_order_details.sql
@@ -5,7 +5,5 @@ CREATE TABLE `order_details` (
   `quantity` int DEFAULT NULL,
    PRIMARY KEY (`id`),
    KEY `my_constraint1` (`product_id`),
-   CONSTRAINT `my_constraint1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
-   KEY `order_id_constraint` (`order_id`),
-   CONSTRAINT `order_id_constraint` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
+   CONSTRAINT `my_constraint1` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=78 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- src/main/resources/db.migration/V16__bookstore_order_status.sql
+++ src/main/resources/db.migration/V16__bookstore_order_status.sql
@@ -2,7 +2,7 @@ CREATE TABLE `order_status` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `order_id` bigint NOT NULL,
   `name_status` varchar(128) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-    KEY `order_id_constraint` (`order_id`),
-    CONSTRAINT `order_id_constraint` FOREIGN KEY (`order_id``) REFERENCES `order` (`id`)
+   PRIMARY KEY (`id`),
+   KEY `order_id_constraint` (`order_id`),
+   CONSTRAINT `order_id_constraint` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
