a5c7830b5cabdcd23ef3fe20a6e0ebcbe8dcc4f3
OleksiiMalchev <oleksiimalchev@gmail.com>
2022-12-26 12:15:00 +0200

    26.12.2022

--- src/main/java/com/javacourse/bookstore/services/impl/BuyServiceImpl.java
+++ src/main/java/com/javacourse/bookstore/services/impl/BuyServiceImpl.java
@@ -1,6 +1,5 @@
 package com.javacourse.bookstore.services.impl;
 
-import com.javacourse.bookstore.domain.OrderDetails;
 import com.javacourse.bookstore.domain.Product;
 import com.javacourse.bookstore.domain.Warehouse;
 import com.javacourse.bookstore.domain.dto.*;
@@ -8,6 +7,7 @@ import com.javacourse.bookstore.mappers.BuyReqDTOToOrderReqDTO;
 import com.javacourse.bookstore.mappers.OrderDetailsMapper;
 import com.javacourse.bookstore.repositories.ProductRepository;
 import com.javacourse.bookstore.services.BuyService;
+import com.javacourse.bookstore.services.OrderDetailsService;
 import com.javacourse.bookstore.services.OrderService;
 import com.javacourse.bookstore.services.ProductService;
 import lombok.RequiredArgsConstructor;
@@ -24,6 +24,7 @@ public class BuyServiceImpl implements BuyService {
     private final ProductRepository productRepository;
     private final OrderDetailsMapper orderDetailsMapper;
     private final ProductService productService;
+    private final OrderDetailsService orderDetailsService;
     @Transactional
     @Override
     public BuyRespDTO buyBook(BuyReqDTO buyReqDTO) {
@@ -31,8 +32,7 @@ public class BuyServiceImpl implements BuyService {
         Long idOrder = order.get().getId();
         OrderDetailsReqDTO orderDetailsReqDTO = buyReqDTOToOrderReqDTO.mapToOrderDetailsReqDTO(buyReqDTO);
         orderDetailsReqDTO.setOrderId(idOrder);
-        OrderDetails orderDetails = orderDetailsMapper.mapToOrderDetails(orderDetailsReqDTO);
-        OrderDetailsRespDTO orderDetailsRespDTO = orderDetailsMapper.mapToOrderDetailsRespDTO(orderDetails);
+       OrderDetailsRespDTO orderDetails = orderDetailsService.createOrderDetails(orderDetailsReqDTO).get();
         Optional<Product> productById = productRepository.findById(buyReqDTO.getProductId());
         Warehouse warehouse = productById.get()
                 .getWarehouses()
@@ -45,7 +45,7 @@ public class BuyServiceImpl implements BuyService {
             warehouse.setReserve(quantityInReq);
         }
         return BuyRespDTO.builder()
-                .orderDetails(orderDetailsRespDTO)
+                .orderDetails(orderDetails)
                 .order(order.get())
                 .productRespDTOWithWarehouseInfo(productService.getProductByIdWithWarehouseInfo(buyReqDTO.getProductId()).get())
                 .build();
